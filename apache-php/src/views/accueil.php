02b54f349b209160857a2d334e81b55624b4cc59
DE BLOTEAU-VAL’CHUK Romain <roro78.asse@gmail.com>
2025-11-18 21:16:24 +0100

    Update accueil.php

--- apache-php/src/views/accueil.php
+++ apache-php/src/views/accueil.php
@@ -3,9 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Géo'scape Game </title>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css">
+    <link rel="stylesheet" href="assets/carte.css">
+
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    <script src="https://unpkg.com/leaflet.heat/dist/leaflet-heat.js"></script>
+
 </head>
 <body>
-    <p>Hello World</p>
+
+    <div id="app">
+
+        <label>
+            <input type="checkbox" v-model="showHeatmap" @change="cocherHeatmap">
+            Afficher la heatmap
+        </label>
+
+    </div>
+
+    <div id="map"></div>
+
+    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+    <script src="assets/carte.js"></script>
 </body>
 </html>
