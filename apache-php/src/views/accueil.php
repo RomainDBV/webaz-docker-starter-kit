fef940f0c36440039eb17bfc9f32285a2e8a1363
DE BLOTEAU-VAL’CHUK Romain <roro78.asse@gmail.com>
2025-11-19 17:22:19 +0100

    Update accueil.php

--- apache-php/src/views/accueil.php
+++ apache-php/src/views/accueil.php
@@ -3,29 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Géo'scape Game </title>
-
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css">
-    <link rel="stylesheet" href="assets/carte.css">
-
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
-    <script src="https://unpkg.com/leaflet.heat/dist/leaflet-heat.js"></script>
-
+    <title>Géo'scape Game - Accueil</title>
+    <link rel="stylesheet" href="assets/accueil.css">
 </head>
 <body>
+    <div class="container">
+        <h1>Bienvenue sur Géo'scape Game !</h1>
 
-    <div id="app">
+        <?php if (isset($error)) : ?>
+            <p class="error"><?= $error ?></p>
+        <?php endif; ?>
 
-        <label>
-            <input type="checkbox" v-model="showHeatmap" @change="cocherHeatmap">
-            Afficher la heatmap
-        </label>
-
+        <form action="/carte" method="post">
+            <label for="pseudo">Entrez votre pseudo :</label>
+            <input type="text" id="pseudo" name="pseudo">
+            <button type="submit">Commencer la partie</button>
+        </form>
     </div>
-
-    <div id="map"></div>
-
-    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
-    <script src="assets/carte.js"></script>
 </body>
 </html>
+
