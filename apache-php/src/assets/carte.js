79578eaae860f15827316578b7fd71b8e3d0f1ad
DE BLOTEAU-VAL’CHUK Romain <roro78.asse@gmail.com>
2025-11-19 17:21:18 +0100

    Update carte.js

--- apache-php/src/assets/carte.js
+++ apache-php/src/assets/carte.js
@@ -1,36 +1,40 @@
-// Création de la carte
-var map = L.map('map').setView([48.85, 2.35], 12);
-
-// Fond OSM
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-var HeatmapLayer = L.tileLayer.wms('http://localhost:8080/geoserver/escapegame/wms', {
-    layers: 'escapegame:objets',
-    styles: 'heatmap',      
-    format: 'image/png',    
-    version: '1.1.0',
-    attribution: '© GeoServer'
-});
-
-
-// Vue app
 Vue.createApp({
     data() {
         return {
-            showHeatmap: false
+            showHeatmap: false,
+            map: null,
+            heatmapLayer: null,
         }
     },
     methods: {
         cocherHeatmap() {
-            if(this.showHeatmap) {
-                HeatmapLayer.addTo(map);
+            if (this.showHeatmap) {
+                this.heatmapLayer.addTo(this.map);
             } else {
-                map.removeLayer(HeatmapLayer);
+                this.map.removeLayer(this.heatmapLayer);
             }
         }
+    },
+    mounted() {
+        // Initialisation de la carte
+        this.map = L.map('map').setView([48.85, 2.35], 12);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(this.map);
+
+        // Layer Heatmap
+        this.heatmapLayer = L.tileLayer.wms('http://localhost:8080/geoserver/escapegame/wms', {
+            layers: 'escapegame:objets',
+            styles: 'heatmap',
+            format: 'image/png',
+            transparent: true,
+            version: '1.1.0',
+            attribution: '© GeoServer'
+        });
     }
 }).mount('#app');
+
+
 
